9588352a9e8cbc4740d4722e31e01ec4a0940ee3
Stephane Carrez <Stephane.Carrez@gmail.com>
2016-09-24 18:55:31 +0200

    Embedded Network Stack root package with global types such as IPv4 address, Ethernet address and pre-defined 1, 2, 4 bytes network types

---
+++ src/net.ads
@@ -0,0 +1,41 @@
+-----------------------------------------------------------------------
+--  net -- Network stack
+--  Copyright (C) 2016 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with Interfaces;
+package Net is
+
+   pragma Pure;
+
+   subtype Uint8 is Interfaces.Unsigned_8;
+   subtype Uint16 is Interfaces.Unsigned_16;
+   subtype Uint32 is Interfaces.Unsigned_32;
+   subtype Uint64 is Interfaces.Unsigned_64;
+
+   --  Length of an IPv4 packet.
+   type Ip_Length is new Uint16;
+
+   --  IPv4 address representation.
+   type Ip_Addr is array (1 .. 4) of Uint8;
+
+   --  Ethernet address representation.
+   type Ether_Addr is array (1 .. 6) of UInt8;
+
+   use type Interfaces.Unsigned_8;
+   use type Interfaces.Unsigned_16;
+   use type Interfaces.Unsigned_32;
+
+end Net;

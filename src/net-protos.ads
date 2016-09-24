5052ac5f73e1f2b556271d4e83e7550af9662d99
Stephane Carrez <Stephane.Carrez@gmail.com>
2016-09-24 18:58:49 +0200

    New package for the root of all protocol implementations

---
+++ src/net-protos.ads
@@ -0,0 +1,24 @@
+-----------------------------------------------------------------------
+--  net-protos -- Network protocols
+--  Copyright (C) 2016 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with Net.Headers;
+package Net.Protos is
+
+   ETHERTYPE_ARP : constant Uint16 := 16#0806#;
+   ETHERTYPE_IP  : constant Uint16 := 16#0800#;
+
+end Net.Protos;

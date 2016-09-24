1021abc8073890d0779180b1308eca35054dfeff
Stephane Carrez <Stephane.Carrez@gmail.com>
2016-09-24 18:56:37 +0200

    New package with To_String operations to convert an IPv4 or Ethernet address to a string

---
+++ src/net-utils.ads
@@ -0,0 +1,27 @@
+-----------------------------------------------------------------------
+--  net-utils -- Network utilities
+--  Copyright (C) 2016 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with Net.Headers;
+package Net.Utils is
+
+   --  Convert the IPv4 address to a dot string representation.
+   function To_String (Ip : in IP_Addr) return String;
+
+   --  Convert the Ethernet address to a string representation.
+   function To_String (Mac : in Ether_Addr) return String;
+
+end Net.Utils;

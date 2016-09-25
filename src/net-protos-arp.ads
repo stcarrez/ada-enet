f8d14a47cc29aca71a07480dd86d928d1c3355fe
Stephane Carrez <Stephane.Carrez@gmail.com>
2016-09-25 23:28:50 +0200

    Fix compilation warning

--- src/net-protos-arp.ads
+++ src/net-protos-arp.ads
@@ -15,7 +15,6 @@
 --  See the License for the specific language governing permissions and
 --  limitations under the License.
 -----------------------------------------------------------------------
-with Net.Headers;
 with Net.Interfaces;
 with Net.Buffers;
 package Net.Protos.Arp is

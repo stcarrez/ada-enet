35e4d975b27995ee28e414f3c6b2e2842f1df7e3
Stephane Carrez <Stephane.Carrez@gmail.com>
2016-11-11 11:29:05 +0100

    Define the Controller task that receive packets

---
+++ demos/echo/receiver.ads
@@ -0,0 +1,36 @@
+-----------------------------------------------------------------------
+--  receiver -- Ethernet Packet Receiver
+--  Copyright (C) 2016 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with System;
+with Net.Interfaces.STM32;
+with Net.Buffers;
+package Receiver is
+
+   --  The Ethernet interface driver.
+   Ifnet     : aliased Net.Interfaces.STM32.STM32_Ifnet;
+
+   --  Maximum number of host we can ping.
+   MAX_PING_HOST : constant Positive := 8;
+
+   NO_IP : constant Net.Ip_Addr := (0, 0, 0, 0);
+
+   --  The task that waits for packets.
+   task Controller with
+     Storage_Size => (16 * 1024),
+     Priority => System.Default_Priority;
+
+end Receiver;

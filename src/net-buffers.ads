94cadcccf904fdbec94d9e84146447d734b73764
Stephane Carrez <Stephane.Carrez@gmail.com>
2016-11-11 09:39:29 +0100

    Declare the Put_Uint16 procedure

--- src/net-buffers.ads
+++ src/net-buffers.ads
@@ -155,6 +155,12 @@ package Net.Buffers is
                         Value : in Net.Uint8) with
      Pre => not Buf.Is_Null;
 
+   --  Add a 16-bit value in network byte order to the buffer data,
+   --  moving the buffer write position.
+   procedure Put_Uint16 (Buf   : in out Buffer_Type;
+                         Value : in Net.Uint16) with
+     Pre => not Buf.Is_Null;
+
    --  Get access to the Ethernet header.
    function Ethernet (Buf : in Buffer_Type) return Net.Headers.Ether_Header_Access with
      Pre => not Buf.Is_Null;

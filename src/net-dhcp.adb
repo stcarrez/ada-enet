14b94c946e5f3ff1214147451e9edbd017e39016
Stephane Carrez <Stephane.Carrez@gmail.com>
2016-11-13 15:18:41 +0100

    Define the DHCP client stub

---
+++ src/net-dhcp.adb
@@ -0,0 +1,33 @@
+-----------------------------------------------------------------------
+--  net-dhcp -- DHCP client
+--  Copyright (C) 2016 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+with Net.Headers;
+with Ada.Real_Time;
+with Net.Interfaces;
+with Net.Buffers;
+with Net.Sockets.Udp;
+package body Net.DHCP is
+
+   overriding
+   procedure Receive (Request  : in out Client;
+                      From     : in Net.Sockets.Sockaddr_In;
+                      Packet   : in out Net.Buffers.Buffer_Type) is
+   begin
+      null;
+   end Receive;
+
+end Net.DHCP;

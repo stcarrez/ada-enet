4d474c9bd43b2193debf5b573e857da7ed95859e
Stephane Carrez <Stephane.Carrez@gmail.com>
2016-11-27 17:27:38 +0100

    Implement the Logger protected operations When we receive a message, add it to the logger

--- demos/echo/echo_server.adb
+++ demos/echo/echo_server.adb
@@ -18,13 +18,51 @@
 
 package body Echo_Server is
 
+   protected body Logger is
+
+      procedure Echo (Content : in Message) is
+      begin
+         Id := Id + 1;
+         if Id <= List'Last then
+            List (Id).Id := Id;
+            List (Id).Content := Content.Content;
+         else
+            List (1 .. List'Last - 1) := List (2 .. List'Last);
+            List (List'Last).Id := Id;
+            List (List'Last).Content := Content.Content;
+         end if;
+      end Echo;
+
+      function Get return Message_List is
+      begin
+         return List;
+      end Get;
+
+   end Logger;
+
    overriding
    procedure Receive (Endpoint : in out Echo_Server;
                       From     : in Net.Sockets.Sockaddr_In;
                       Packet   : in out Net.Buffers.Buffer_Type) is
+      use type Net.Uint16;
+
+      Size   : constant Net.Uint16 := Packet.Get_Data_Size;
       Status : Net.Error_Code;
+      Len    : Natural;
+      Msg    : Message;
+      Skip   : Net.Uint16;
    begin
+      if Size > Msg.Content'Length then
+         Skip := Net.Uint16 (Len) - Msg.Content'Length;
+         Len := Msg.Content'Length;
+      else
+         Skip := 0;
+         Len := Natural (Size);
+      end if;
+      Packet.Get_String (Msg.Content (1 .. Len));
+      Packet.Set_Data_Size (Size);
       Endpoint.Count := Endpoint.Count + 1;
+      Endpoint.Messages.Echo (Msg);
       Endpoint.Send (To => From, Packet => Packet, Status => Status);
    end Receive;
 

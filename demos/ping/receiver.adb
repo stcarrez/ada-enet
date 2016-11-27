1f950c1582023ae6c345a396149c00684d94eede
Stephane Carrez <Stephane.Carrez@gmail.com>
2016-11-27 11:46:58 +0100

    Use the Ifnet instance provided by the Demos package

--- demos/ping/receiver.adb
+++ demos/ping/receiver.adb
@@ -21,6 +21,8 @@ with Net.Protos.Arp;
 with Net.Protos.Icmp;
 with Net.Protos.IPv4;
 with Net.Headers;
+with Net.Interfaces;
+with Demos;
 package body Receiver is
 
    use type Net.Ip_Addr;
@@ -122,7 +124,7 @@ package body Receiver is
          exit when Packet.Is_Null;
 
          Packet.Set_Length (64);
-         Net.Protos.Icmp.Echo_Request (Ifnet     => Ifnet,
+         Net.Protos.Icmp.Echo_Request (Ifnet     => Demos.Ifnet,
                                        Target_Ip => Hosts (I).Ip,
                                        Packet    => Packet,
                                        Seq       => Hosts (I).Seq,
@@ -177,7 +179,7 @@ package body Receiver is
       Packet  : Net.Buffers.Buffer_Type;
       Ether   : Net.Headers.Ether_Header_Access;
    begin
-      while not Ifnet.Is_Ready loop
+      while not Demos.Ifnet.Is_Ready loop
          delay until Ada.Real_Time.Clock + Ada.Real_Time.Seconds (1);
       end loop;
       loop
@@ -185,12 +187,12 @@ package body Receiver is
             Net.Buffers.Allocate (Packet);
          end if;
          if not Packet.Is_Null then
-            Ifnet.Receive (Packet);
+            Demos.Ifnet.Receive (Packet);
             Ether := Packet.Ethernet;
             if Ether.Ether_Type = Net.Headers.To_Network (Net.Protos.ETHERTYPE_ARP) then
-               ARP_Input (Ifnet, Packet);
+               ARP_Input (Demos.Ifnet, Packet);
             elsif Ether.Ether_Type = Net.Headers.To_Network (Net.Protos.ETHERTYPE_IP) then
-               IP_Input (Ifnet, Packet);
+               IP_Input (Demos.Ifnet, Packet);
             end if;
          else
             delay until Ada.Real_Time.Clock + Ada.Real_Time.Milliseconds (100);

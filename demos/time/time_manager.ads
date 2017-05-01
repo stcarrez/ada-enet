ca420ef58220a5898b7d5c6af2d78e99628255f9
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-05-01 18:06:55 +0200

    Declare a Client_Type that overrides the DNS to override the Answer operation and configure the NTP when we resolve the NTP server name

--- demos/time/time_manager.ads
+++ demos/time/time_manager.ads
@@ -17,8 +17,36 @@
 -----------------------------------------------------------------------
 
 with Net.NTP;
+with Net.DNS;
 package Time_Manager is
 
-   Client : aliased Net.NTP.Client;
+   type Client_Type is new Net.DNS.Query with record
+      Server : aliased Net.NTP.Client;
+      Port   : Net.Uint16 := Net.NTP.NTP_PORT;
+   end record;
+
+   --  Save the answer received from the DNS server.  This operation is called for each answer
+   --  found in the DNS response packet.  The Index is incremented at each answer.  For example
+   --  a DNS server can return a CNAME_RR answer followed by an A_RR: the operation is called
+   --  two times.
+   --
+   --  This operation can be overriden to implement specific actions when an answer is received.
+   overriding
+   procedure Answer (Request  : in out Client_Type;
+                     Status   : in Net.DNS.Status_Type;
+                     Response : in Net.DNS.Response_Type;
+                     Index    : in Natural);
+
+   --  NTP client based on the NTP server provided by DHCP option.
+   Client     : aliased Net.NTP.Client;
+
+   --  NTP client to the "ntp.ubuntu.com" server.
+   Ubuntu_Ntp : aliased Client_Type;
+
+   --  NTP client to the "pool.ntp.org" server (at least one of them).
+   Pool_Ntp   : aliased Client_Type;
+
+   --  NTP client to the "ntp.bouyguesbox.fr" server.
+   Bbox_Ntp   : aliased Client_Type;
 
 end Time_Manager;

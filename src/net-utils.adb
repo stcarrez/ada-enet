79976d877ec5e84913b49156e63fbefe5d3a2ec5
Stephane Carrez <Stephane.Carrez@gmail.com>
2017-06-05 17:58:54 +0200

    Make the Random function a separate function from the Net.Utils package

--- src/net-utils.adb
+++ src/net-utils.adb
@@ -23,6 +23,9 @@ package body Net.Utils is
 
    Hex_String : constant String := "0123456789ABCDEF";
 
+   --  Get a 32-bit random number.
+   function Random return Uint32 is separate;
+
    function Hex (Value : in Uint8) return String is
       use Interfaces;
 

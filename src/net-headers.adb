1a71e98b515932a3ee67a46368e2d3559500eeb0
Maxim Reznik <reznikmm@gmail.com>
2024-07-26 09:18:02 +0300

    Implement To_Host/To_Network for Uint32

--- src/net-headers.adb
+++ src/net-headers.adb
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  net-headers -- Network headers
---  Copyright (C) 2016 Stephane Carrez
+--  Copyright (C) 2016-2024 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -22,8 +22,11 @@ package body Net.Headers is
    --  Convert integers to network byte order.
    --  ------------------------------
    function To_Network (Val : in Uint32) return Uint32 is
+      Upper : constant Uint16 := To_Network (Uint16 (Val and 16#FFFF#));
+      Lower : constant Uint16 :=
+        To_Network (Uint16 (Interfaces.Shift_Right (Val, 16)));
    begin
-      return Val;
+      return Interfaces.Shift_Left (Uint32 (Upper), 16) or Uint32 (Lower);
    end To_Network;
 
    function To_Network (Val : in Uint16) return Uint16 is
@@ -34,14 +37,8 @@ package body Net.Headers is
    --  ------------------------------
    --  Convert integers to host byte order.
    --  ------------------------------
-   function To_Host (Val : in Uint32) return Uint32 is
-   begin
-      return Val;
-   end To_Host;
+   function To_Host (Val : in Uint32) return Uint32 renames To_Network;
 
-   function To_Host (Val : in Uint16) return Uint16 is
-   begin
-      return Interfaces.Rotate_Left (Val, 8);
-   end To_Host;
+   function To_Host (Val : in Uint16) return Uint16 renames To_Network;
 
 end Net.Headers;

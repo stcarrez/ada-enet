cd33351b598567e941b3d1f8fe779f632cad3ffe
Maxim Reznik <reznikmm@gmail.com>
2024-07-26 12:51:44 +0300

    Move STM32 driver to `drivers/enet_stm32/`
    and wrap it as an Alire crate. Drop ADL dependency by providing
    our-own copy of ethernet SVD file.

--- demos/utils/demos.adb
+++ demos/utils/demos.adb
@@ -156,6 +156,11 @@ package body Demos is
       --  Setup some receive buffers and initialize the Ethernet driver.
       Net.Buffers.Add_Region (STM32.SDRAM.Reserve (Amount => HAL.UInt32 (NET_BUFFER_SIZE)),
                               NET_BUFFER_SIZE);
+
+      Ifnet.Configure
+        (Pins => Net.STM32_Interfaces.STM32F42X_Pins,
+         RMII => True);
+
       Ifnet.Initialize;
       Receiver.Start;
 
